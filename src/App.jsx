33dc8c617efb1f0d8a846f47fa8722b9f4eb746d
iovann <iovannatcho@gmail.com>
2024-07-31 11:33:35 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+function App() {
+
+  return (
+    <>
+     <div className="container">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic commodi nihil necessitatibus itaque, veniam facilis quos recusandae voluptatum molestias voluptas error fuga impedit totam est natus fugiat nisi tempore alias.
+     </div>
+    </>
+  )
+}
+
+export default App
