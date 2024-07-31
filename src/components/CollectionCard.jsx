e92d9b89371f2f3ede6963e3eb414e39c8b1ca8e
iovann <iovannatcho@gmail.com>
2024-07-31 22:08:02 +0100

    animations

--- src/components/CollectionCard.jsx
+++ src/components/CollectionCard.jsx
@@ -1,22 +1,24 @@
-import React from 'react'
+import React from 'react';
 
-const CollectionCard = ({ image, cardTitle }) => {
+const CardComponent = ({ image, cardTitle }) => {
     return (
         <div className="col-lg-3 col-md-6 col-9">
-            <div className="card rounded-4">
-                <img src={image} className="card-img-top img-fluid collectionImg object-fit-cover p-2 rounded-5" alt="Collection Image" />
-                <div className="card-body">
-                    <div className="card-text d-flex justify-content-between fw-semibold">
-                        <span className='fw-bold'>{cardTitle}</span>
-                        <span>
-                            <img src="./assets/icons/ethereum.svg" alt="" className='img-fluid' /> 68
-                        </span>
+            <div className="card">
+                <div className="card2">
+                    <img src={image} className="card-img-top img-fluid collectionImg object-fit-cover p-2 rounded-5" alt="Collection Image" />
+                    <div className="card-body">
+                        <div className="card-text d-flex justify-content-between fw-semibold">
+                            <span className='fw-bold'>{cardTitle}</span>
+                            <span>
+                                <img src="./assets/icons/ethereum.svg" alt="" className='img-fluid' /> 68
+                            </span>
+                        </div>
+                        <button className='btn w-100 btn btn-light border border-1 border-black mt-3'>Place a Bid</button>
                     </div>
-                    <button className='btn w-100 btn btn-light border border-1 border-black mt-3'>Place a Bid</button>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default CollectionCard
+export default CardComponent;
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { motion } from 'framer-motion';
+
 
 const HeroSection = () => {
     return (
-        <div className='row mb-5'>
-            <div className="col-lg-7 typewriter">
+        <motion.div className='row mb-5'
+            initial={{ opacity: 0, y: 150 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+        >
+            <div className="col-lg-7">
                 <h1 className='mainTitle fw-normal neutral-600'>See the NFT new world</h1>
                 <p className='w-75'>Vorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, di</p>
                 <p>
@@ -13,19 +19,27 @@ const HeroSection = () => {
             </div>
             <div className="col-lg-5 heroImage">
                 <div className="row pt-5">
-                    <div className="col-6">
+                    <motion.div className="col-6"
+                        initial={{ opacity: 0, y: 50 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 1.5 }}
+                    >
                         <p className='text-end'>
                             <img src="./assets/icons/crown.svg" alt="" className='img-fluid' /><br />
                         </p>
                         <img src="./assets/images/machine.png" alt="" className='rounded-top-pill rounded-bottom-pill img-fluid' />
-                    </div>
+                    </motion.div>
 
-                    <div className="col-6">
+                    <div className="col-6"
+                        initial={{ opacity: 0, y: 50 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 1.5 }}
+                    >
                         <img src="./assets/images/monkey.png" alt="" className='rounded-top-pill rounded-bottom-pill img-fluid' />
                     </div>
                 </div>
             </div>
-        </div>
+        </motion.div>
     )
 }
 
