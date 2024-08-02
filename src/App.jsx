6b884defbfcab9012cf9695fa00e01c322f9b751
iovann <iovannatcho@gmail.com>
2024-08-02 15:59:20 +0100

    correction

--- src/App.jsx
+++ src/App.jsx
@@ -43,11 +43,14 @@ function App() {
           </div>
         </AnimatedSection>
 
-        <div className="build py-5 w-100 text-center lh-1 d-flex flex-column align-items-center my-3 rounded-5">
-          <p className="text-white display-1 fw-bold">Build your NFT profile</p><br />
-          <p className="text-white fw-bold">Join almost 10k NFT profiles on Digit !</p><br />
-          <button className="btn btn-light px-3 fw-semibold rounded-pill">Sign up now</button>
+        <div className="ps-2 ps-lg-0">
+          <div className="build py-5 w-100 text-center lh-1 d-flex flex-column align-items-center my-3 rounded-5">
+            <p className="text-white display-1 fw-bold">Build your NFT profile</p><br />
+            <p className="text-white fw-bold">Join almost 10k NFT profiles on Digit !</p><br />
+            <button className="btn btn-light px-3 fw-semibold rounded-pill">Sign up now</button>
+          </div>
         </div>
+
       </section>
       <Footer />
     </>
